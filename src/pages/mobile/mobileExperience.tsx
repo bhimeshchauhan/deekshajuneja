609f12106009ac227fd3c0f3f98579e0815c5f22
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-01-27 13:02:39 +0530

    file-abstraction
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- src/pages/mobile/mobileExperience.tsx
+++ src/pages/mobile/mobileExperience.tsx
@@ -1,105 +1,18 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Social } from "../../components/socials/socials";
 import { Layout } from "../../components/layout";
 import { ExperienceItem } from "../../data/experience";
-
-export const ExpContainer = styled.div`
-  display: flex;
-  margin-top: 7vw;
-  height: 95%;
-  flex-direction: column;
-`;
-
-export const ExpCompanyDetails = styled.div`
-  height: 65%;
-  display: flex;
-  align-items: flex-end;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 2%;
-  flex: 1;
-  border-top-right-radius: 35px;
-  border-bottom-right-radius: 35px;
-`;
-
-export const ExpResp = styled.div`
-  height: 65%;
-  background: white;
-  flex: 1;
-  align-content: flex-end;
-  display: flex;
-  flex-direction: column;
-`;
-
-export const CompanyLogo = styled.div<{ bg }>`
-  position: absolute;
-  height: 30%;
-  width: 30%;
-  background-image: ${(props) => `url(${props.bg})`};
-  top: 21.5%;
-  left: 30%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  filter: drop-shadow(2px 4px 6px black);
-`;
-
-export const ExpInfo = styled.div`
-  align-self: flex-start;
-  > h1 {
-    font-size: 1.7rem;
-  }
-`;
-
-export const ExpInfoText = styled.h4`
-  margin-bottom: 0;
-  width: 82%;
-`;
-
-export const RespList = styled.ul`
-  margin-top: 20vw;
-  margin-left: 14vw;
-  margin-right: 20vw;
-  overflow: auto;
-  filter: drop-shadow(2px 4px 6px black);
-  > li {
-    font-style: italic;
-  }
-`;
-
-export const Controls = styled.div`
-  display: flex;
-  width: 28%;
-  align-items: flex-start;
-  justify-content: space-between;
-  margin-top: 2%;
-`;
-
-export const ControlButton = styled.button`
-  display: flex;
-  width: 10vw;
-  height: 10vw;
-  align-items: center;
-  justify-content: space-around;
-  border-radius: 30px;
-  border: none;
-  cursor: pointer;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  background: white;
-  margin: 4% 0;
-  align-items: center;
-  :active {
-    border-style: outset;
-  }
-  font-style: montserrat;
-  font-weight: 700;
-  line-height: 21px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  text-transform: uppercase;
-`;
+import {
+  ExpContainer,
+  ExpCompanyDetails,
+  ExpInfo,
+  ExpInfoText,
+  Controls,
+  ControlButton,
+  ExpResp,
+  RespList,
+  CompanyLogo,
+} from "../../styles/mobile/mobileExperience.styles";
 
 export const MobileExperience = () => {
   const [expIndex, setExpIndex] = useState(1);
--- src/pages/web/webExperience.tsx
+++ src/pages/web/webExperience.tsx
@@ -1,174 +1,20 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Social } from "../../components/socials/socials";
 import { Layout } from "../../components/layout";
-import Carousel from "nuka-carousel";
 import { ExperienceItem } from "../../data/experience";
-
-export const SocialContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
-
-export const ExpContainer = styled.div`
-  display: flex;
-  margin: 7vw;
-  flex-direction: column;
-  @media (min-width: 1440px) {
-    margin-top: 4vw;
-  }
-`;
-
-export const Line = styled.ul<{ index: number }>`
-  display: flex;
-  margin: 0;
-  height: fit-content;
-  align-items: center;
-  & li:nth-of-type(${(props) => props.index + 1}) {
-    transform: scale(2);
-    transition-duration: 0.5s;
-    margin: 0 5vw;
-  }
-`;
-
-export const Divider = styled.hr`
-  width: 100%;
-  margin: 0;
-`;
-
-export const LineItem = styled.li`
-  list-style: none;
-  height: 4vw;
-  margin: 0;
-  margin: 0 2vw;
-  align-items: center;
-  display: flex;
-  transition-duration: 0.5s;
-`;
-
-export const ListDetail = styled.div<{ img }>`
-  display: flex;
-  flex-direction: column;
-  width: 1vw;
-  height: 1vw;
-  border-radius: 1vw;
-  animation: pulse 5s;
-  transform: scale(6);
-  margin: 0 2vw;
-  background-image: ${(props) => `url(${props.img})`};
-  background-size: 80%;
-  background-repeat: no-repeat;
-  background-position: center;
-  :hover {
-    cursor: pointer;
-  }
-`;
-
-export const Card = styled.div`
-  background-color: white;
-  margin: 0 2vw;
-`;
-
-export const ExpCarousel = styled(Carousel)`
-  margin-top: 7vh;
-  .slider-frame {
-    height: 60vh !important;
-    background-color: white;
-    box-shadow: 0px 0px 42px 7px rgba(0, 0, 0, 0.15);
-    margin: 0 12px;
-    border-radius: 25px;
-  }
-  .slider-control-centerleft {
-    left: -6% !important;
-    button {
-      background-color: transparent;
-      height: 30px;
-      width: 30px;
-      border-radius: 50%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      :hover {
-        cursor: pointer;
-      }
-    }
-  }
-  .slider-control-centerright {
-    right: -6% !important;
-    button {
-      background-color: transparent;
-      height: 30px;
-      width: 30px;
-      border-radius: 50%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      :hover {
-        cursor: pointer;
-      }
-    }
-  }
-`;
-
-export const EmploymentCardContainer = styled.div`
-  margin: 10.2%;
-`;
-
-export const Responsibility = styled.div`
-  overflow: auto;
-  height: 20vh;
-  font-style: italic;
-  margin-top: 10%;
-  background: /* Shadow covers */ linear-gradient(
-      white 30%,
-      rgba(255, 255, 255, 0)
-    ),
-    linear-gradient(rgba(255, 255, 255, 0), white 70%) 0 100%,
-    /* Shadows */
-      radial-gradient(
-        50% 0,
-        farthest-side,
-        rgba(0, 0, 0, 0.2),
-        rgba(0, 0, 0, 0)
-      ),
-    radial-gradient(
-        50% 100%,
-        farthest-side,
-        rgba(0, 0, 0, 0.2),
-        rgba(0, 0, 0, 0)
-      )
-      0 100%;
-  background: /* Shadow covers */ linear-gradient(
-      white 30%,
-      rgba(255, 255, 255, 0)
-    ),
-    linear-gradient(rgba(255, 255, 255, 0), white 70%) 0 100%,
-    /* Shadows */
-      radial-gradient(
-        farthest-side at 50% 0,
-        rgba(0, 0, 0, 0.2),
-        rgba(0, 0, 0, 0)
-      ),
-    radial-gradient(
-        farthest-side at 50% 100%,
-        rgba(0, 0, 0, 0.2),
-        rgba(0, 0, 0, 0)
-      )
-      0 100%;
-  background-repeat: no-repeat;
-  background-color: white;
-  background-size: 100% 40px, 100% 40px, 100% 14px, 100% 14px;
-  /* Opera doesn't support this in the shorthand */
-  background-attachment: local, local, scroll, scroll;
-`;
-
-export const Header = styled.h2`
-  margin-bottom: 2%;
-`;
-
-export const Title = styled.h4`
-  margin-bottom: 1%;
-`;
+import {
+  EmploymentCardContainer,
+  Title,
+  Responsibility,
+  ExpCarousel,
+  Line,
+  Divider,
+  LineItem,
+  ListDetail,
+  ExpContainer,
+  SocialContainer,
+  Header,
+} from "../../styles/web/webExperience.styles";
 
 export const EmploymentCard = ({ employment }) => {
   return (
