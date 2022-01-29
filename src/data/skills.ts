513396f6343cfe4487781caac2f404ad8c432232
bhimeshchauhan <bhimeshchauhan@gmail.com>
2022-01-29 13:24:06 +0530

    add-packages-deployment
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

---
+++ src/data/skills.ts
@@ -0,0 +1,28 @@
+import {
+  faJira,
+  faSlack,
+  faSalesforce,
+} from "@fortawesome/free-brands-svg-icons";
+
+export const BusinessSkills = [
+  {
+    label: "Jira",
+    icon: faJira,
+  },
+  {
+    label: "Slack",
+    icon: faSlack,
+  },
+  {
+    label: "Salesforce",
+    icon: faSalesforce,
+  },
+  {
+    label: "Slack",
+    icon: faSlack,
+  },
+  {
+    label: "Salesforce",
+    icon: faSalesforce,
+  },
+];
--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,10 +1,31 @@
 import * as React from "react";
+import styled from "styled-components";
 import { Layout } from "../components/layout";
+import { BusinessSkills } from "../data/skills";
+
+export const CardContainer = styled.div`
+  height: 100%;
+  padding: 10vw;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export const Card = styled.div`
+  height: 37vh;
+  width: 10vw;
+  background-color: orangered;
+  flex: 0 0 32%;
+`;
 
 const SkillsPage = () => {
   return (
     <Layout bgcolor={"#FFF"}>
-      <div />
+      <CardContainer>
+        {BusinessSkills.map(() => {
+          return <Card />;
+        })}
+      </CardContainer>
     </Layout>
   );
 };
